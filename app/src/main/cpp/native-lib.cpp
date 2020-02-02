d961f6aad16ceb22b65903cbd168b8045fc96ea3
Alex Almeida Tavella <alexkotr@gmail.com>
2020-02-02 14:32:31 -0300

    Removing `app` legacy folder

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,6 +0,0 @@
-#include <jni.h>
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_br_com_alex_moov_data_tmdb_TMDBApiKeyHolder_getApiKey(JNIEnv *env) {
-    return env->NewStringUTF("12893b65fa1774765a53d7b9a1b07b70");
-}
