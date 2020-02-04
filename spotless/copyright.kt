1517ebc36c110fcef7966a7befd44ac214b43d40
Alex Almeida Tavella <alexkotr@gmail.com>
2020-02-04 00:47:06 -0300

    Adding copywright and spotless config for it

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Alex Almeida Tavella
+ * Copyright $YEAR Alex Almeida Tavella
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package br.com.moov.app.core
-
-open class UiModel(val error: Throwable? = null)
